2b66807fac8d6c81c9c5196fc989a9c3f0c4caa7
dknyakundi <dknyakundi1@gmail.com>
2024-12-17 15:45:35 +0300

    first commit

---
+++ server/app.ts
@@ -0,0 +1,37 @@
+import express, { NextFunction, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { ErrorMiddleware } from "./utils/middleware/error";
+
+require("dotenv").config();
+
+export const app = express();
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors = Cross Origin Resource Sharing
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// testing API
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    Message: "API is working",
+  });
+});
+
+// unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+});
+
+app.use(ErrorMiddleware);
